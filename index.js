c0fcfd401534088ad63cd252c49360eb59ba0456
Himansh <himanshbansal200521@gmail.com>
2025-08-25 10:17:49 +0530

    setup full duplex connection practicing emit, broadcast using socket and io

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const port = 3000;
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+
+    socket.on('msg_send', (data) => {
+        console.log(data);
+        io.emit('msg_recv', data);
+    });
+});
 
 app.use('/', express.static(__dirname + '/public'));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server started`);
 });
